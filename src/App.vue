96985252f3dfb36c8631c5c3876cd825786680ab
paix <ghiwotselam@gmail.com>
2020-09-24 20:57:12 +0300

    Heroku config

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from "./components/Login.vue";
 
 export default Vue.extend({
   name: "App",
