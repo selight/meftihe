1294f931095041093d0911847776f256cf5a7bbf
melica101 <amalicamelly@gmail.com>
2020-09-25 10:47:12 +0300

    Vuetify type error solved

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,17 @@
 <template>
-  <v-container>
-    <v-layout row class="text-xs-center">
-      <v-flex xs3>
-        <v-card height="500px"></v-card>
+  <v-container class="mt-12" fluid>
+    <v-layout align-center="center" justify-center="center" row wrap>
+      <v-flex class="md3 xs12 sm4 lg6 justify-center mb-6">
+        <v-img class="mx-auto mb-12" src="../assets/logo.png"></v-img>
       </v-flex>
-      <v-flex xs4 class="grey lighten-4">
-        <v-container
-          style="position: relative;top: 50%;"
-          class="text-xs-center"
-        >
-          <v-card flat>
-            <v-card-actions>
-              <v-btn primary large block>Login</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-container>
-      </v-flex>
+    </v-layout>
+
+    <v-layout row wrap class="justify-center align-center mt-12">
+      <v-btn x-large color="#fcca14" dark>
+        <v-avatar height="30px" width="30px" class="mr-3"
+          ><img src="../assets/google.png"/></v-avatar
+        >Login with Google</v-btn
+      >
     </v-layout>
   </v-container>
 </template>
