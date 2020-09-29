7a119aef3c9e10e472fc65125b76d9226dff043b
amalica <amalicamelly@gmail.com>
2020-09-29 15:49:27 +0300

    search animated

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,12 +1,6 @@
 <template>
   <div
-    class=".search_bar"
-    style="
-    position: absolute;
-    top: 40%;
-    left: 50%;
-    margin: 0 -50% 0 0;
-    transform: translate(-50%, -50%)"
+    class="search"
   >
     <v-text-field
       prepend-inner-icon="mdi-magnify"
@@ -14,15 +8,47 @@
       solo
       flat
       hide-details
+      @focus="isSearching = true"
+      @blur="isSearching = false"
+      v-bind:class="isSearching ? 'search-top' : 'search-center'"
     ></v-text-field>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: "home"
+  name: "home",
+  data: function() {
+    return {
+      isActive: true,
+      isSearching: false,
+      name: "vue-js"
+    };
+  },
+  methods: {
+    toggleClass: function() {
+      this.isActive = !this.isActive;
+    },
+    searchClass: function() {
+      this.isSearching = !this.isSearching;
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.search-top {
+  top: 5%;
+  left: 50%;
+  margin: 0 -50% 0 0;
+  transform: translate(-50%, -50%);
+  position: absolute;
+}
+.search-center {
+  position: absolute;
+  top: 40%;
+  left: 50%;
+  margin: 0 -50% 0 0;
+  transform: translate(-50%, -50%)
+}
+</style>
