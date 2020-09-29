16bdd751c52f6ae0ff9a7dcc9d5bc0838d7f9e79
melica101 <amalicamelly@gmail.com>
2020-09-29 13:22:46 +0300

    Home page done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <v-img src="../assets/back.jpeg" min-height="100%">
+    <div class="home">
+      <search />
+      <v-fab-transition>
+        <v-btn
+          v-show="!hidden"
+          color="#fcca14"
+          dark
+          absolute
+          bottom
+          right
+          class="mb-12"
+          fab
+        >
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </v-fab-transition>
+    </div>
+  </v-img>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
+import search from "../components/search.vue";
 @Component({
   components: {
-    HelloWorld
+    search
   }
 })
 export default class Home extends Vue {}
