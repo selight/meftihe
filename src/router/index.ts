dbee5ae81d9f33a578c3950e666533d79ed89b71
amalica <amalicamelly@gmail.com>
2020-10-01 17:30:57 +0300

    problem page done

--- src/router/index.ts
+++ src/router/index.ts
@@ -19,6 +19,15 @@ const routes: Array<RouteConfig> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/Home.vue")
+  },
+  {
+    path: "/problem",
+    name: "Problem",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+        import(/* webpackChunkName: "about" */ "../views/problem.vue")
   }
 ];
 
